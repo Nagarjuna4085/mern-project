bdf0758220d0b85132b4e8e6a65dea50434a8447
Nagarjuna4085 <nagarjuna.sanem@gmail.com>
2022-01-26 17:39:23 +0530

    posts ,post,form and respected styles,material-ui importe

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import {Container,AppBar,Typography,Grow,Grid} from '@material-ui/core';
+import Posts from './components/Posts/Posts'
+import Form from './components/Form/Form'
+import memories from './images/memories.png'
+import useStyles from './styles';
 const App = () =>{
+    const classes= useStyles();
 return(
-    <div>
-      <h1>App</h1>
-    </div>
+    <container maxidth="lg" >
+        <AppBar className={classes.appBar} position="static" color="inherit" >
+            <Typography className={classes.heading} varient="h1" align="center" > Memories</Typography>
+            <img className={classes.image} src={memories} alt="memories"  height="60" />
+        </AppBar>
+        <Grow in >
+            <containe>
+                <Grid container justify="space-between" alignItems='strech' spacing={3}>
+                       <Grid item xs={12} sm={7}>
+                            <Posts />
+                        </Grid>
+                        <Grid item xs={12} sm={4}  >
+                            <Form />
+                        </Grid>
+                </Grid>
+            </containe>
+         
+        </Grow>
+
+    </container>
 );
 }
 export default App;
