5fed49f8b94fa965e89243ab76efacf2e36ad512
Nagarjuna4085 <nagarjuna.sanem@gmail.com>
2022-01-27 00:21:39 +0530

    create action is created

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,9 +1,9 @@
  const reducer = (posts=[],action) =>{
     switch(action.type){
-        case "FETCH_ALL":
+        case 'FETCH_ALL':
             return action.payload;
-        case 'CREATE' :
-            return posts;
+        case 'CREATE':
+            return [...posts, action.payload];
         default :
             return posts;
     }
