50d80c8d620a83fa14df92c8489e7b5426be1af8
Nagarjuna4085 <nagarjuna.sanem@gmail.com>
2022-01-27 17:17:14 +0530

    form props changes done

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -17,17 +17,17 @@ const Form = () => {
 
      }
      const clear =() => {
-
+        
      }
 
     return(
         <Paper className={classes.paper}>
             <form autoComplete='off' noValidate className={`${classes.root} ${classes.form}`}  onSubmit={handleSubmit}>
                 <Typography varient="h6">Creating a Memory</Typography>
-                <TextField   name="creator" varient="outlined"label="Creator" value={postData.creator}onChange={(e)=> setPostData({...postData,creator:e.target.value})} fullWidthvalue />
-                <TextField   name="title" varient="outlined"label="Title" value={postData.title}onChange={(e)=> setPostData({...postData,title:e.target.value})} fullWidthvalue />
-                <TextField   name="msg" varient="outlined"label="Message" value={postData.message}onChange={(e)=> setPostData({...postData,message:e.target.value})} fullWidthvalue />
-                <TextField   name="ta"gs varient="outlined"label="Tags" value={postData.tags}onChange={(e)=> setPostData({...postData,tags:e.target.value})} fullWidthvalue />
+                <TextField   name="creator" varient="outlined"label="Creator" value={postData.creator}onChange={(e)=> setPostData({...postData,creator:e.target.value})} fullwidthvalue="true" />
+                <TextField   name="title" varient="outlined"label="Title" value={postData.title}onChange={(e)=> setPostData({...postData,title:e.target.value})} fullwidthvalue="true" />
+                <TextField   name="msg" varient="outlined"label="Message" value={postData.message}onChange={(e)=> setPostData({...postData,message:e.target.value})} fullwidthvalue="true" />
+                <TextField   name="ta"gs varient="outlined"label="Tags" value={postData.tags}onChange={(e)=> setPostData({...postData,tags:e.target.value})} fullwidthvalue="true" />
                 <div className={classes.fileInput}> <FileBase multiple={false} onDone={({base64}) => setPostData({...postData,selectedFile:base64})}/></div>
                 <Button className={classes.buttonSubmit} varient="contained" color="primary" size="large" type="submit" fullWidth >Submit</Button>
                 <Button  varient="contained" color="secondary" size="small" onClick={clear} fullWidth >Clear</Button>
